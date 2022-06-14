2f63582df268183dd27298353560cc18255a8cf0
Reginaldo Miranda <reginaldobrain@gmail.com>
2022-06-14 16:31:11 -0300

    firma 1631

--- src/sisbar/model/ModelPdv.java
+++ src/sisbar/model/ModelPdv.java
@@ -17,6 +17,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import org.eclipse.persistence.annotations.Noncacheable;
 
 /**
  *
