689cacd87341ca3057ba6e8c4bf73ef6cdb1b1a1
Reginaldo Miranda <reginaldobrain@gmail.com>
2022-06-20 11:41:04 -0300

    firma 1141

---
+++ src/sisbar/model/MoVenda.java
@@ -0,0 +1,164 @@
+
+package sisbar.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "vendas")
+        
+public class MoVenda implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data" , nullable = false)
+    private Calendar data;
+    @Column(name= "valor_total", nullable = false, columnDefinition = "decimal(12,2)")
+    private Double valorTotal;
+    @Column(name = "parcelas" , nullable = false)
+    private Integer parcelas;
+    @ManyToOne
+    @JoinColumn(name = "cliente", referencedColumnName = "id", nullable = false)
+    private ModelClientes moclientes;
+    
+    @OneToMany(mappedBy = "venda" , cascade = CascadeType.ALL , orphanRemoval = false, fetch = FetchType.LAZY)
+    private List<MoVendaItens> itens = new ArrayList<>();
+
+    public MoVenda() {
+        this.valorTotal = 0.0;
+    }
+
+    public void adicionarItens(MoVendaItens obj){
+        obj.setVenda(this);
+        this.valorTotal += obj.getValorTotal();
+        this.itens.add(obj);
+        
+    }
+    public void removerItens(int index){
+          MoVendaItens obj = this.itens.get(index);
+          this.valorTotal = obj.getValorTotal();
+          this.itens.remove(index);
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the data
+     */
+    public Calendar getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the parcelas
+     */
+    public Integer getParcelas() {
+        return parcelas;
+    }
+
+    /**
+     * @param parcelas the parcelas to set
+     */
+    public void setParcelas(Integer parcelas) {
+        this.parcelas = parcelas;
+    }
+
+    /**
+     * @return the moclientes
+     */
+    public ModelClientes getMoclientes() {
+        return moclientes;
+    }
+
+    /**
+     * @param moclientes the moclientes to set
+     */
+    public void setMoclientes(ModelClientes moclientes) {
+        this.moclientes = moclientes;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MoVenda other = (MoVenda) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    /**
+     * @return the itens
+     */
+    public List<MoVendaItens> getItens() {
+        return itens;
+    }
+
+    /**
+     * @param itens the itens to set
+     */
+    public void setItens(List<MoVendaItens> itens) {
+        this.itens = itens;
+    }
+    
+}
---
+++ src/sisbar/model/MoVendaItens.java
@@ -0,0 +1,141 @@
+
+package sisbar.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "venda_itens")
+public class MoVendaItens implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "quantidade", nullable = false)
+    private Double quantidade;
+    @Column(name = "valorUnitario")
+    private Double valorUnitario;
+    @Column(name = "valorTotal")
+    private Double valorTotal;
+    @ManyToOne
+    @JoinColumn(name = "venda", referencedColumnName = "id" )
+    private MoVenda venda;
+    @ManyToOne
+    @JoinColumn(name = "produto" , referencedColumnName = "id")
+    private ModelProdutos produtos;
+
+    public MoVendaItens() {
+    }
+
+ 
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valorUnitario
+     */
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    /**
+     * @param valorUnitario the valorUnitario to set
+     */
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the venda
+     */
+    public MoVenda getVenda() {
+        return venda;
+    }
+
+    /**
+     * @param venda the venda to set
+     */
+    public void setVenda(MoVenda venda) {
+        this.venda = venda;
+    }
+
+    /**
+     * @return the produtos
+     */
+    public ModelProdutos getProdutos() {
+        return produtos;
+    }
+
+    /**
+     * @param produtos the produtos to set
+     */
+    public void setProdutos(ModelProdutos produtos) {
+        this.produtos = produtos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MoVendaItens other = (MoVendaItens) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+}
